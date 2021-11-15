0509c4140ec95a6add9a548ff10903621a5af42b
Nursurtan <arapovnur23@gmail.com>
2021-11-15 20:05:07 +0700

    Initial Commit

---
+++ Tic Tac Toe Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tic Tac Toe Game
+//
+//  Created by нурсултан арапов on 11/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

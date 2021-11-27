f4fabe7725fe3694ecf0609c080037444d882223
Nursurtan <arapovnur23@gmail.com>
2021-11-27 23:23:33 +0700

    reset draw

--- Tic Tac Toe Game/ViewController.swift
+++ Tic Tac Toe Game/ViewController.swift
@@ -33,15 +33,44 @@ class ViewController: UIViewController {
     
     var NOUGHT = "O"
     var CROSS = "X"
-    
+    var board = [UIButton]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initBoard()
         // Do any additional setup after loading the view.
+    }
+    func initBoard() {
+        board.append(a1)
+        board.append(a2)
+        board.append(a3)
+        board.append(b1)
+        board.append(b2)
+        board.append(b3)
+        board.append(c1)
+        board.append(c2)
+        board.append(c3)
     }
 
     @IBAction func boardTapAction(_ sender: UIButton) {
         addToBoard(sender)
+        if(fullBoard()) {
+            
+        }
+    }
+    
+    func resultAlert(title: String) {
+        let ac = UIAlertController(title: title, message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Reset", style: .default, handler: {(_) in } ))
+    }
+    
+    func fullBoard() -> Bool {
+        for button in board {
+            if button.title(for: .normal) == nil {
+                return false
+            }
+        }
+        return true
     }
     
     func addToBoard(_ sender: UIButton) {
@@ -56,6 +85,7 @@ class ViewController: UIViewController {
                 currentTurn = Turn.Nought
                 turnLabel.text = NOUGHT
             }
+            sender.isEnabled = false
         }
     }
 }
